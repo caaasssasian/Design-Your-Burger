c1426fca0370a9abf3b161266daf360b58203b2e
caaasssasian <cassidyctho@hotmail.com>
2017-03-10 20:45:05 -0500

    created error and 404 pages

---
+++ 404.php
@@ -0,0 +1,6 @@
+<main class="container">
+    <section class="jumbotron">
+        <h1>Oh no!</h1>
+        <p>Sorry, but we cannot find the page you are looking for. Please check the URL again and re-enter.</p>
+    </section>
+</main>
---
+++ error.php
@@ -0,0 +1,6 @@
+<main class="container">
+    <section class="jumbotron">
+        <h1>Sorry!</h1>
+        <p>It seems that we have an error. We'll solve it as quickly as possible.</p>
+    </section>
+</main>
